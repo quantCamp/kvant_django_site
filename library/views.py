01a248f3489eaf5a0700bc5f65dd724e8632fbad
pyhoner0 <ramil.chepkunov@gmail.com>
2026-07-07 15:59:03 +0300

    Надо сделать ДБ

--- library/views.py
+++ library/views.py
@@ -4,4 +4,9 @@ from library import models
 
 def main_page(request):
     categories = models.Category.objects.all()
-    return render(request, "./library.html", context=categories)
+    return render(request, "./library.html", context={"categories": categories})
+
+
+def category_page(request, category):
+    materials = models.Material.objects.filter(category=category)
+    return render(request, "./category.html", context={"materials": materials})
