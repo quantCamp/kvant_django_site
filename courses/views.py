3740c36ebb5a138d8aa152968acbf9fd413d5834
mnikonov982-pixel <mnikonov982@gmail.com>
2026-07-07 12:26:08 +0300

    models

---
+++ courses/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from .models import Course
+
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
+    context_object_name = 'courses'
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course_detail.html'
+    context_object_name = 'course'
