6c00d3e4317d8e4abf4643c8cb57c039684a6354
fiVEs6 <secondgrogu@gmail.com>
2026-07-08 15:20:09 +0300

    gallery

--- quant_site/urls.py
+++ quant_site/urls.py
@@ -21,11 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('gallery/', include('gallery.urls')),
+    path('', include('main.urls')),           # Главная страница
+    path('gallery/', include('gallery.urls')), # Галерея
 ]
 
-# ВАЖНО: Добавить обработку медиа-файлов
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
